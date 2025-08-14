dbc18ca938f81e94564fd12aa6261ac438c1b0c3
tikrack <tikrackcode@gmail.com>
2025-08-14 21:44:54 +0330

    [Sefver] -> create first node project

---
+++ main.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+
+function processView (fileName) {
+    return "./html/" + (fileName.trim() === "" ? "index" : fileName.trim()) + ".html"
+}
+
+const server = http.createServer((req, res) => {
+    const fileName = req.url.substring(1)
+
+    if (fileName.split(".").length > 1) {
+        return;
+    }
+
+    const html = fs.readFileSync(processView(fileName), 'utf8')
+
+    res.setHeader("Content-Type", "text/html");
+    res.write(html)
+
+    res.end()
+})
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening on 8000...");
+});
